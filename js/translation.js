4c858dea0c7950c413729aed1afcece546a3b999
KOUACHI Randa <renda.kouachi@gmail.com>
2024-04-29 15:25:20 +0100

    traduction suite

--- js/translation.js
+++ js/translation.js
@@ -4,6 +4,216 @@ const dictionnaire = {
         fr: "Accueil",
         en: "Home",
         ar: "الرئيسية",
+    },
+    "Fabrication des accessoires en plastiques": {
+        "fr": "Fabrication des accessoires en plastiques",
+        "en": "Manufacturing of plastic accessories",
+        "ar": "تصنيع الإكسسوارات البلاستيكية"
+    },
+    "a propos": {
+        "fr": "À propos",
+        "en": "About",
+        "ar":" من نحن"
+    },
+    "categories": {
+        "fr": "Catégories",
+        "en": "Categories",
+        "ar": "الفئات"
+    },
+    "contact": {
+        "fr": "Contact",
+        "en": "Contact",
+        "ar": "اتصل بنا"
+    },
+    "Des solutions durables": {
+        "fr": "Des solutions durables",
+        "en": "Sustainable solutions",
+        "ar": "حلول مستدامة"
+    },
+    "pour votre cuisine": {
+        "fr": "pour votre cuisine",
+        "en": "for your kitchen",
+        "ar": "لمطبخك"
+    },
+    "nos": {
+        "fr": "Nos",
+        "en": "Our",
+        "ar": ""
+    },
+    "Explorez notre sélection de produits soigneusement conçus pour répondre à divers besoins.": {
+        "fr": "Explorez notre sélection de produits soigneusement conçus pour répondre à divers besoins.",
+        "en": "Explore our carefully crafted selection of products to meet various needs.",
+        "ar": "استكشف اختيارنا الدقيق للمنتجات المصممة بعناية لتلبية احتياجات متنوعة."
+    },
+    "Volants Cocotte": {
+        "fr": "Volants Cocotte",
+        "en": "Cocotte Wheels",
+        "ar": "عجلات القدر"
+    },
+    "description volants cocotte": {
+        "fr": "Conçus avec précision par HB industrie, fusionnent l'élégance du plastique et la robustesse de l'acier.",
+        "en": "Precision-designed by HB Industry, merging the elegance of plastic with the robustness of steel.",
+        "ar": "صُممت بدقة من قبل شركة HB الصناعية ، مما يجمع بين أناقة البلاستيك وقوة الفولاذ."
+    },
+    "Voir Produits": {
+        "fr": "Voir Produits",
+        "en": "View Products",
+        "ar": "عرض المنتجات"
+    },
+    "Bras Cocotte": {
+        "fr": "Bras Cocotte",
+        "en": "Cocotte Arms",
+        "ar": "أذرع القدر"
+    },
+    "description bras cocotte": {
+        "fr": "Conçus avec minutie par HB Industrie, marient l'élégance du plastique à la robustessede l'acier, offrant ainsi une alliance parfaite entre style et durabilité.",
+        "en": "Meticulously designed by HB Industry, they marry the elegance of plastic with the robustness of steel, offering a perfect blend of style and durability.",
+        "ar": "مُصممة بعناية فائقة من قبل شركة HB Industrie ، تجمع بين أناقة البلاستيك وقوة الفولاذ ، مما يوفر مزيجًا مثاليًا من الأناقة والمتانة."
+    },
+    "Accessoires Poele": {
+        "fr": "Accessoires Poele",
+        "en": "Pan Accessories",
+        "ar": "ملحقات المقلاة"
+    },
+    "description accessoires poele": {
+        "fr": "Poignées adaptées aux casseroles et poêles de la gamme HB Phenol POIGNÉE ANTIDÉRAPANTE CONFORTABLE Fabriquée en matériau anti-brûlure en bakélite à haute résistance, cette poignée est solide et durable, capable de résister à l'usure d'une utilisation quotidienne car elle vous servira pendant longtemps.",
+        "en": "Handles suitable for pans and pots from the HB Phenol range COMFORTABLE NON-SLIP HANDLE Made of high-strength heat-resistant bakelite material, this handle is sturdy and durable, capable of withstanding the wear and tear of daily use as it will serve you for a long time.",
+        "ar": "مقابض مناسبة لمقالي وأواني من مجموعة HB Phenol مقبض مريح غير قابل للانزلاق مصنوع من مادة الباكيليت عالية القوة ومقاومة للحرارة ، هذا المقبض قوي ودائم ، قادر على تحمل الاهتراء الناتج عن الاستخدام اليومي حيث سيخدمك لفترة طويلة."
+    },
+    "Bras Casserole": {
+        "fr": "Bras Casserole",
+        "en": "Casserole Arms",
+        "ar": "أذرع القدر"
+    },
+    "description bras casserole": {
+        "fr": "Conçus avec précision par HB Industrie, fusionnent l'élégance du plastique et la robustesse de l'acier.",
+        "en": "Precision-designed by HB Industry, merging the elegance of plastic with the robustness of steel.",
+        "ar": "مصممة بدقة من قبل HB Industry ، تجمع بين أناقة البلاستيك وقوة الفولاذ."
+    },
+    "Roue De Chariot": {
+        "fr": "Roue De Chariot",
+        "en": "Cart Wheel",
+        "ar": "عجلة العربة"
+    },
+    "description roue de chariot": {
+        "fr": "Roues solides en acier inoxydable, résistantes aux températures élevées car fabriquées dans un matériau incombustible, un type de plastique appelé bakélite, utilisé pour les boîtes, les outils, les fours et les équipements industriels. Remplacement silencieux des roues et utilisation garantie dans votre maison et votre lieu de travail. Conçu avec soin par HB penoul.",
+        "en": "Solid stainless steel wheels, resistant to high temperatures as they are made of a non-combustible material, a type of plastic called bakelite, used for boxes, tools, ovens, and industrial equipment. Silent replacement of wheels and guaranteed use in your home and workplace. Carefully designed by HB Penoul.",
+        "ar": "عجلات من الفولاذ المقاوم للصدأ الصلبة ، مقاومة لدرجات الحرارة العالية حيث يتم تصنيعها من مادة غير قابلة للاشتعال ، وهي نوع من البلاستيك يُسمى الباكيليت ، والذي يستخدم في الصناديق والأدوات والأفران والمعدات الصناعية. استبدال صامت للعجلات واستخدام مضمون في منزلك ومكان عملك. مصمم بعناية من قبل HB Penoul."
+    },
+    "Passoire Lavabo": {
+        "fr": "Passoire Lavabo",
+        "en": "Sink Strainer",
+        "ar": "مصفاة بالوعة"
+    },
+    "description passoire lavabo": {
+        "fr": "Les filtres fabriqués par HP Phénol se caractérisent par leur haute qualité et vous permettent de profiter longtemps du produit. Fabriqué en acier inoxydable de haute qualité, ce qui le rend durable et résistant à toutes les conditions, la structure à mailles fines de la crépine emprisonne la saleté, les résidus alimentaires, les cheveux et autres particules empêchant les blocages dans vos tuyaux de vidange.",
+        "en": "Filters made by HP Phenol are characterized by their high quality and allow you to enjoy the product for a long time. Made of high-quality stainless steel, making it durable and resistant to all conditions, the fine mesh structure of the strainer traps dirt, food residues, hair, and other particles preventing blockages in your drainage pipes.",
+        "ar": "يتميز المرشحون الذين تصنعهم HP Phenol بجودتهم العالية ويتيحون لك الاستمتاع بالمنتج لفترة طويلة. مصنوعة من الفولاذ المقاوم للصدأ عالي الجودة ، مما يجعله متينًا ومقاومًا لجميع الظروف ، تحتجز الهيكلة الدقيقة للمصفاة الأوساخ وبقايا الطعام والشعر والجسيمات الأخرى مما يمنع تسد في أنابيب الصرف الخاصة بك."
+    },
+    "avantages": {
+        "fr": "Avantages",
+        "en": "Advantages",
+        "ar": "المميزات"
+    },
+    "Découvrez pourquoi nous sommes la meilleure solution pour répondre à vos besoins.": {
+        "fr": "Découvrez pourquoi nous sommes la meilleure solution pour répondre à vos besoins.",
+        "en": "Discover why we are the best solution to meet your needs.",
+        "ar": "اكتشف لماذا نحن الحل الأفضل لتلبية احتياجاتك."
+    },
+    "qualité": {
+        "fr": "Qualité",
+        "en": "Quality",
+        "ar": "الجودة"
+    },
+    "description qualité": {
+        "fr": "Notre engagement envers l'excellence dans la sélection des matériaux garantit que nos produits offrent une qualité exceptionnelle à nos clients.",
+        "en": "Our commitment to excellence in material selection ensures that our products deliver outstanding quality to our customers.",
+        "ar": "يضمن التزامنا بالتميز في اختيار المواد أن منتجاتنا تقدم جودة استثنائية لعملائنا."
+    },
+    "Durabilité": {
+        "fr": "Durabilité",
+        "en": "Durability",
+        "ar": "المتانة"
+    },
+    "description Durabilité": {
+        "fr": "Notre utilisation des meilleurs matériaux, notamment de l'acier et du plastique de haute qualité, garantit non seulement une performance optimale, mais aussi une durabilité exceptionnelle.",
+        "en": "Our use of the finest materials, including high-quality steel and plastic, ensures not only optimal performance but also exceptional durability.",
+        "ar": "استخدامنا لأفضل المواد ، بما في ذلك الفولاذ عالي الجودة والبلاستيك ، لا يضمن فقط أداءً مثاليًا ولكن أيضًا متانة استثنائية."
+    },
+    "Satisfaction Client": {
+        "fr": "Satisfaction Client",
+        "en": "Customer Satisfaction",
+        "ar": "رضا العملاء"
+    },
+    "description Satisfaction Client": {
+        "fr": "Votre satisfaction est notre priorité absolue. Nous nous efforçons de fournir des produits de qualité supérieure qui répondent à vos besoins et dépassent vos attentes.",
+        "en": "Your satisfaction is our top priority. We strive to provide superior quality products that meet your needs and exceed your expectations.",
+        "ar": "رضاكم هو أولويتنا. نحن نسعى جاهدين لتوفير منتجات عالية الجودة تلبي احتياجاتكم وتتجاوز توقعاتكم."
+    },
+    "Service client exceptionnel": {
+        "fr": "Service client exceptionnel",
+        "en": "Exceptional Customer Service",
+        "ar": "خدمة عملاء استثنائية"
+    },
+    "description Service client exceptionnel": {
+        "fr": "Nous sommes là pour vous aider à chaque étape du processus.",
+        "en": "We are here to assist you at every step of the process.",
+        "ar": "نحن هنا لمساعدتك في كل خطوة من العملية."
+    },
+    "Catalogue de Produits": {
+        "fr": "Catalogue de Produits",
+        "en": "Product Catalog",
+        "ar": "كتالوج المنتجات"
+    },
+    "description catalogue": {
+        "fr": "Vous pouvez également consulter notre gamme de produits",
+        "en": "You can also browse our range of products",
+        "ar": "يمكنك أيضًا استعراض مجموعة منتجاتنا"
+    },
+    "telecharger": {
+        "fr": "Télécharger",
+        "en": "Download",
+        "ar": "تحميل"
+    },
+    "qui sommes": {
+        "fr": "Qui sommes",
+        "en": "Who are",
+        "ar": "من"
+    },
+    "nous ?": {
+        "fr": "Nous ?",
+        "en": "We ?",
+        "ar": "نحن ؟"
+    },
+    "description a propos": {
+        "fr": "La société Cuicul HB Phenol a été fondée à El Eulma en Algérie au début du XXe siècle, où elle se limitait initialement à la vente en gros. Par la suite, elle est devenue un fabricant majeur d'ustensiles de cuisine. Cuicul HB Phenol est une entreprise familiale dont le succès repose sur une histoire, un parcours et des valeurs. C'est la première entreprise à avoir investi dans ce domaine, en s'appuyant sur des talents algériens, qu'ils soient ingénieurs, administrateurs ou artisans, et en s'engageant dans leur formation continue pour les amener, ainsi que l'entreprise, à des niveaux avancés de développement et à la production de produits de haute qualité conformes aux normes internationales.",
+        "en": "The Cuicul HB Phenol company was founded in El Eulma, Algeria, in the early 20th century, initially focusing on wholesale trade. Later, it became a major manufacturer of kitchen utensils. Cuicul HB Phenol is a family-owned business whose success is based on history, journey, and values. It was the first company to invest in this field, relying on Algerian talents, whether engineers, administrators, or artisans, and committing to their continuous training to bring them, as well as the company, to advanced levels of development and production of high-quality products that meet international standards.",
+        "ar": "تأسست شركة Cuicul HB Phenol في العلمة ، الجزائر ، في أوائل القرن العشرين ، حيث كانت في البداية تركز على التجارة بالجملة. في وقت لاحق ، أصبحت شركة تصنيع رئيسية لأدوات المطبخ. تعتبر Cuicul HB Phenol شركة عائلية نجاحها مبني على التاريخ والرحلة والقيم. كانت أول شركة تستثمر في هذا المجال ، بالاعتماد على المواهب الجزائرية ، سواء كانوا مهندسين أو مسؤولين أو حرفيين ، والتفاني في تدريبهم المستمر لجلبهم ، بالإضافة إلى الشركة ، إلى مستويات متقدمة من التطوير وإنتاج منتجات عالية الجودة تتوافق مع المعايير الدولية."
+    },
+    "Conseil d'": {
+        "fr": "Conseil",
+        "en": "Board of",
+        "ar": "مجلس"
+    },
+    "administration": {
+        "fr": "administration",
+        "en": "Directors",
+        "ar": "الإدارة"
+    },
+    "description conseil": {
+        "fr": "Composé de professionnels chevronnés et engagés qui guident stratégiquement notre entreprise vers le succès.",
+        "en": "Comprised of seasoned and dedicated professionals who strategically guide our company towards success.",
+        "ar": "مكون من محترفين مجربين وملتزمين يوجهون بشكل استراتيجي شركتنا نحو النجاح."
+    },
+    "Hammani Lahbib":{
+        fr:"Hammani Lahbib",
+        en:"Hammani Lahbib",
+        ar:"حماني لحبيب"
+    },
+    "gerant":{
+        fr:"Gérant de l'entreprise",
+        en:"Business Manager",
+        ar: "مدير الشركة"
     }
 }
 
@@ -12,13 +222,13 @@ let nodes = document.querySelectorAll('[data-lang]');
 
 selector.addEventListener("change", (e)=>{
     localStorage.setItem("lang",e.target.value)
-    let language = selector.options[selector.selectedIndex].value;
-    translation(language)
+    translation(e.target.value)
     
 })
 
 document.addEventListener('DOMContentLoaded',()=>{
-    const language = localStorage.getItem("lang");
+    const language = localStorage.getItem("lang")||"fr";
+    console.log(language);
     translation(language)
     selector.value= language
 })
@@ -29,5 +239,6 @@ const translation =(langue)=>{
         nodes[index].innerHTML = dictionnaire[key][langue]
         
     }
+    document.dir = langue === "ar" ? "rtl" : "ltr";
 }
 
